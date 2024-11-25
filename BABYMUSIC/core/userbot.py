924660c5f7d63a6ad2a9e7a664381cf5b6c4fc6d
rockykartik <115865655+rockykartik@users.noreply.github.com>
2024-11-25 15:32:20 +0530

    Update userbot.py

--- BABYMUSIC/core/userbot.py
+++ BABYMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/+OL6jdTL7JAJjYzVl")
-                await self.one.join_chat("BABY09_WORLD")
+                await self.one.join_chat("https://t.me/about_arthik")
+                await self.one.join_chat("about_arthik")
             except:
                 pass
             assistants.append(1)
